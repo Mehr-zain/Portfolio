dd892c35f23e53a72181d9d3d054122cf443be08
Mehr-zain <meharzain0017@gmail.com>
2024-09-24 14:14:29 +0500

    Updated Resume, Icon,

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -128,8 +128,8 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/src/assets/Zain_ResumeF.pdf' 
-              download='Zain_Resume.pdf' 
+              href='/assets/Zain_ResumeF.pdf'
+              download='Zain_Resume.pdf'
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
